f96e65d31402c19018a038aaf967e81ef3d010c4
Christian Ramos <christivn.ramos@gmail.com>
2021-02-21 01:49:28 +0100

    Update cliente.lua

--- cliente.lua
+++ cliente.lua
@@ -16,50 +16,50 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 
-        while ESX == nil do
-            TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
-            Citizen.Wait(0)
-        end
+		while ESX == nil do
+		    TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+		    Citizen.Wait(0)
+		end
 
-        for i, punto in ipairs(puntos) do
-            local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-            DrawMarker(38, punto.x, punto.y, punto.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 59, 169, 219, 50, false, true, 2, nil, nil, false)
-            DrawMarker(27, punto.x, punto.y, punto.z-1, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 2.0, 2.0, 2.0, 59, 169, 219, 50, false, true, 2, nil, nil, false)
-            
-            if IsPedInAnyVehicle(PlayerPedId(), false) and matriculaBici==nil then
-		local matriculaGenerada = math.random(0,99999)
-                local bici = GetVehiclePedIsIn(PlayerPedId(), false)
-                ESX.Game.SetVehicleProperties(bici, { plate = matriculaGenerada })
-                matriculaBici = ESX.Game.GetVehicleProperties(bici).plate
-            end
+		for i, punto in ipairs(puntos) do
+		    local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+		    DrawMarker(38, punto.x, punto.y, punto.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 59, 169, 219, 50, false, true, 2, nil, nil, false)
+		    DrawMarker(27, punto.x, punto.y, punto.z-1, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 2.0, 2.0, 2.0, 59, 169, 219, 50, false, true, 2, nil, nil, false)
 
-            if GetDistanceBetweenCoords(punto.x, punto.y, punto.z-1, x, y, z) < 1.5 and tieneBicicleta==false then
-                Draw3DText( punto.x, punto.y, punto.z, 2, "Pulsa ~b~E ~w~para alquilar una bicicleta por ~b~"..precio.."€")
-                if IsControlJustPressed(1,38) then
-                    spawn_bicicleta()
-                    TriggerServerEvent("bicicletas:cobrar")
-                    ESX.ShowNotification("Has alquilado una bicicleta por ~g~"..precio.."€")
-                end
+		    if IsPedInAnyVehicle(PlayerPedId(), false) and matriculaBici==nil then
+			local matriculaGenerada = math.random(0,99999)
+			local bici = GetVehiclePedIsIn(PlayerPedId(), false)
+			ESX.Game.SetVehicleProperties(bici, { plate = matriculaGenerada })
+			matriculaBici = ESX.Game.GetVehicleProperties(bici).plate
+		    end
 
-            elseif GetDistanceBetweenCoords(punto.x, punto.y, punto.z-1, x, y, z) < 1.5 and tieneBicicleta==true then
-                Draw3DText( punto.x, punto.y, punto.z, 2, "Pulsa ~b~E ~w~para devolver la bicicleta")
-                if IsControlJustPressed(1,38) then
-                    local bici = GetVehiclePedIsIn(GetPlayerPed(-1), false)
-                    local biciData = ESX.Game.GetVehicleProperties(bici)
+		    if GetDistanceBetweenCoords(punto.x, punto.y, punto.z-1, x, y, z) < 1.5 and tieneBicicleta==false then
+			Draw3DText( punto.x, punto.y, punto.z, 2, "Pulsa ~b~E ~w~para alquilar una bicicleta por ~b~"..precio.."€")
+			if IsControlJustPressed(1,38) then
+			    spawn_bicicleta()
+			    TriggerServerEvent("bicicletas:cobrar")
+			    ESX.ShowNotification("Has alquilado una bicicleta por ~g~"..precio.."€")
+			end
 
-                    if biciData.plate==matriculaBici then
-                        TriggerServerEvent("bicicletas:entregar")
-                        ESX.Game.DeleteVehicle(bici)
-                        tieneBicicleta=false
-                        ESX.ShowNotification("Has entregado la bicicleta, y se te a devuelto ~g~"..math.ceil(precio/2).."€")
-                    else
-                        ESX.ShowNotification("~r~Esta no es la bicicleta que alquilastes")
-                    end
+		    elseif GetDistanceBetweenCoords(punto.x, punto.y, punto.z-1, x, y, z) < 1.5 and tieneBicicleta==true then
+			Draw3DText( punto.x, punto.y, punto.z, 2, "Pulsa ~b~E ~w~para devolver la bicicleta")
+			if IsControlJustPressed(1,38) then
+			    local bici = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+			    local biciData = ESX.Game.GetVehicleProperties(bici)
 
-                end
-            end
+			    if biciData.plate==matriculaBici then
+				TriggerServerEvent("bicicletas:entregar")
+				ESX.Game.DeleteVehicle(bici)
+				tieneBicicleta=false
+				ESX.ShowNotification("Has entregado la bicicleta, y se te a devuelto ~g~"..math.ceil(precio/2).."€")
+			    else
+				ESX.ShowNotification("~r~Esta no es la bicicleta que alquilastes")
+			    end
 
-        end
+			end
+		    end
+
+		end
 
     end
 end)
